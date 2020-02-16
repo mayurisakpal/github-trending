9a5cb1220cb225cf45642b3eb2c111fb0d6d6616
mayurisakpal <mayusakpal910@gmail.com>
2020-02-16 12:48:55 +0530

    Updates

---
+++ precache-manifest.e6f32f8f921e329b29fce9b235794949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f26a97fd4454dae29d4644f1069eb3",
+    "url": "/github-trending/index.html"
+  },
+  {
+    "revision": "b1d17546749ab222de7c",
+    "url": "/github-trending/static/css/main.c0e26233.chunk.css"
+  },
+  {
+    "revision": "c612fa4494d4eb9e3c35",
+    "url": "/github-trending/static/js/2.9e25b4b3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/github-trending/static/js/2.9e25b4b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1d17546749ab222de7c",
+    "url": "/github-trending/static/js/main.0f38ebdb.chunk.js"
+  },
+  {
+    "revision": "32970fd6b492f1177902",
+    "url": "/github-trending/static/js/runtime-main.7f6026d7.js"
+  }
+]);
